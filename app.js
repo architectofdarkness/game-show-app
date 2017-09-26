681eaeeb0e20d4d2f58506c8162d5728aea0f9c6
Samuel Cleophas <samuel.skater@gmail.com>
2017-09-27 09:21:21 +1000

    Sprint 50 | Improved Button Checking Functions

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-const querty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
 const reset = document.querySelector('a.btn__reset');
-
-const phrases = [
-  'OLD MCDONALD HAD A FARM',
-  'WHAT DO YOU MEAN',
-  'NO WAY DUDE',
-  'I LIKE TRAINS',
-  'THERES AN APP FOR THAT'
-];
-let missed = 0;
-
-const getRandomPhraseAsArray = (arr) => {
-  length = arr.length;
-  
-  let randomPhrase = arr[Math.floor(length * Math.random())];
-  let letters = randomPhrase.split('');
-  return letters;
-}
-
-const addPhraseToDisplay = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    const ul = document.querySelector('#phrase ul');
-    
-    let li = document.createElement('li');
-    li.textContent = arr[i];
-    li.className = "letter";
-    ul.appendChild(li);
-  }
-}
 
 reset.addEventListener('click', () => {
   const overlay = document.querySelector('#overlay');
@@ -37,5 +7,85 @@ reset.addEventListener('click', () => {
 
 });
 
+
+const phrases = [
+  'MY NAME IS JEFF',
+  'WHAT DO YOU MEAN',
+  'NO WAY DUDE',
+  'I LIKE TRAINS',
+  'CASEY NEISTAT'
+];
+
+const phrase = document.getElementById('phrase');
+const phraseUL = document.querySelector('#phrase ul');
+
+const qwerty = document.getElementById('qwerty');
+
+let missed = 0;
+
+
+
+const getRandomPhraseAsArray = (arr) => {
+  length = arr.length;
+  
+  let randomPhrase = arr[Math.floor(length * Math.random())];
+  let letters = randomPhrase.split('');
+  return letters;
+};
+
+const addPhraseToDisplay = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = arr[i];
+    
+    if (arr[i] !== " ") {
+      li.classList.add("letter");
+    } else {
+      li.classList.add("space");
+    }
+    phraseUL.appendChild(li);
+  }
+};
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
+// Check if a
+const checkLetter = (button) => {
+  const letterList = document.querySelectorAll('#phrase .letter');
+  
+  
+  for (let i = 0; i < letterList.length; i++) {
+    let letterListItem = letterList[i];
+    let displayLetter = letterListItem.textContent;
+    let qwertyLetter = button.textContent.toUpperCase();
+    
+    if (displayLetter == qwertyLetter) {
+      letterListItem.classList.add("show");
+      return displayLetter;
+    } else {
+      return null;
+    }
+  }
+};
+
+qwerty.addEventListener('click', (event) => {
+  if (event.target.tagName == "BUTTON") {
+    let button = event.target;
+    button.classList.add("chosen");
+    button.disabled = "true";
+    checkLetter(button);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
